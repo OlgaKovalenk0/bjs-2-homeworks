893f9d2ce1a7104ec7a31328c95dbf1c6e17d40d
Olga Kovalenko <korog87@mail.ru>
2024-06-09 23:01:29 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
+let student1 = new Student("Ольга", "женский", 36);
+let student2 = new Student("Александр", "мужской", 45);
+let student3 = new Student("Наталья", "женский", 24);
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks !== undefined) {
+        this.marks = this.marks.concat(marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks === undefined || this.marks.length === 0) {
+        return 0;
+    } else {
+        return this.marks.reduce((acc, mark) => acc + mark, 0) / this.marks.length;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
